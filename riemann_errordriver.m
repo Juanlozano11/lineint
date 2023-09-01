4fc6b9c45b80430c02ca56d3444799d65ee11ac4
Juanlozano11 <Juanes.lozano2003@gmail.com>
2023-09-01 10:03:17 -0700

    Update riemann_errordriver.m

--- riemann_errordriver.m
+++ riemann_errordriver.m
@@ -8,10 +8,20 @@ max_n = 100;
 % Get the errors from the error_riemann function
 errors = riemann_error(a, b, max_n);
 
-% Now, plot the errors in the driver script with the desired appearance
+% Plot the linear scale errors in the first figure
 figure;
-plot(1:max_n, errors, 'o', 'LineWidth', 2); % Using circles ('bo') for points
+
+plot(1:max_n, errors, 'o', 'LineWidth', 2);
 xlabel('Number of subdivisions (n)');
 ylabel('Error');
-title('Error in Riemann Sum Approximation');
+title('Error in Riemann Sum Approximation (Linear Scale)');
+grid on;
+
+% Plot the log-log scale errors in a new figure
+figure;
+
+loglog(1:max_n, errors, 'o', 'LineWidth', 2);
+xlabel('Number of subdivisions (n)');
+ylabel('Error');
+title('Error in Riemann Sum Approximation (Log-Log Scale)');
 grid on;
