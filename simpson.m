5c4e12424da6cbd5985a53de330997eb9b776b02
Juanlozano11 <Juanes.lozano2003@gmail.com>
2023-09-13 20:34:18 -0700

    formating

--- simpson.m
+++ simpson.m
@@ -6,18 +6,23 @@ function s = simpson(g, a, b, n)
     if mod(n,2) == 1
         n = n + 1;
     end
+
     % Compute the width of each subinterval
     h = (b - a) / n;
+    
     % Initialize result
     s = g(a) + g(b);
+    
     % Summation for odd indices
     for i = 1:2:n-1
         s = s + 4 * g(a + i * h);
     end
+
     % Summation for even indices
     for i = 2:2:n-2
         s = s + 2 * g(a + i * h);
     end
+    
     % Finalize the result
     s = (h / 3) * s;
 end
